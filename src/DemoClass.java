04c1d0a4d77354cca553597b54a8137d0fc5905b
nabeelmahmud <nabeel_mahmud@hotmail.com>
2021-10-31 23:35:13 -0400

    new Sout statement

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -1,6 +1,8 @@
 public class DemoClass {
     public static void main(String[] args) {
         System.out.println("this is a test file");
+        System.out.println("test");
+        System.out.println("A new change");
 
     }
 }
